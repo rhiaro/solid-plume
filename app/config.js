09310b51962a4c53a61f889fb80e1930109e9088
Amy Guy <rhiaro+gh@gmail.com>
2015-12-07 11:03:39 -0800

    Update config.js

--- app/config.js
+++ app/config.js
@@ -8,5 +8,5 @@ Plume.config = {
     tagline: "Light as a feather",
     picture: "img/logo-white.svg",
     defaultPath: 'posts',
-    dataContainer: 'https://data.amy.gy'
+    dataContainer: 'https://data.amy.gy/blog/'
 };
